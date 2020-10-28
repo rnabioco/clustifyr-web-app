9ee640cec57477e3e33a7b386740e8cacd3ed93a
Sidhant Puntambekar <sidhantnp@yahoo.com>
2020-10-28 08:25:27 -0600

    Added drop down menu for clustifyrdatahub references

--- scRNA-seq-atlas/app.R
+++ scRNA-seq-atlas/app.R
@@ -4,6 +4,7 @@ library(readr)
 library(tools)
 library(clustifyr)
 library(rsconnect)
+#library(ExperimentHub)
 options(shiny.maxRequestSize = 1500*1024^2)
 options(repos = BiocManager::repositories())
 options(shiny.reactlog = TRUE)
@@ -64,7 +65,15 @@ ui <- fluidPage(
                                      All = "all"),
                          selected = "head"),
             
-            textInput("metadataCellType", label = h3("Metadata Cell Type"), placeholder = "Enter metadata cell type column")
+            textInput("metadataCellType", label = h3("Metadata Cell Type"), placeholder = "Enter metadata cell type column"),
+            
+            selectInput("dataHubReference", "ClustifyrDataHub Reference:", 
+                        choices=list("ref_MCA","ref_tabula_muris_drop","ref_tabula_muris_facs",
+                        "ref_mouse.rnaseq", "ref_moca_main", "ref_immgen", "ref_hema_microarray",
+                        "ref_cortex_dev", "ref_pan_indrop", "ref_pan_smartseq2", 
+                        "ref_mouse_atlas")),
+            hr(),
+            helpText("Choose cell reference for clustify function")
             
         ),
         
@@ -234,6 +243,11 @@ server <- function(input, output) {
         reference_matrix <- average_clusters(mat = data1(), metadata = data2()[[input$metadataCellType]], if_log = TRUE)
         head(reference_matrix)
     })
+    
+    #eh <- ExperimentHub()
+    ## query
+    #refs <- query(eh, "clustifyrdatahub")
+    #refs
     
 }
 
